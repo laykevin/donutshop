116d3988363a76755de3de35f60d10aa2348242f
Kevin Lay <kevinlay@live.com>
2023-08-16 01:30:25 -0700

    Finished added animation to carousel.

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,25 +1,35 @@
 import { useCallback, useEffect, useState } from "react";
 import { GoDot, GoDotFill } from "react-icons/go";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import { TMenuItemCategory } from "../lib";
+import { ButtonLink } from ".";
 
 export const Carousel = () => {
   const [isShowing, setIsShowing] = useState(0);
 
-  const images = [
+  const images: {
+    src: string;
+    bgColor: string;
+    title: string;
+    buttonLink: TMenuItemCategory;
+  }[] = [
     {
       src: "https://images.unsplash.com/photo-1602080926844-153f7ff5ce66?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
       bgColor: "red",
       title: "So Many Donuts!",
+      buttonLink: "Donuts",
     },
     {
       src: "https://images.unsplash.com/photo-1527904324834-3bda86da6771?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1104&q=80",
       bgColor: "lightblue",
       title: "Fresh Brewed Coffee!",
+      buttonLink: "Coffee & Drinks",
     },
     {
       src: "https://images.unsplash.com/photo-1574694571857-6e28ed21dfe2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
       bgColor: "orange",
       title: "Check Out Our Breakfast!",
+      buttonLink: "Breakfast",
     },
   ];
 
@@ -40,6 +50,7 @@ export const Carousel = () => {
     src: string;
     bgColor: string;
     title: string;
+    buttonLink: TMenuItemCategory;
   }) => {
     return (
       <>
@@ -48,13 +59,16 @@ export const Carousel = () => {
           className="flex flex-col justify-around items-center self-stretch min-w-full py-5 bg-rose-600 text-white px-10"
           style={{ backgroundColor: carouselData.bgColor }}
         >
-          <h1>{carouselData.title}</h1>
+          <h1 style={{ fontFamily: "Francois One" }}>{carouselData.title}</h1>
           <span style={{ fontFamily: "Ubuntu Condensed", fontSize: "1.5rem" }}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
             tempore provident quia nostrum iure minima suscipit nam totam
             similique perferendis, alias vel et? Tempora possimus rem soluta qui
             provident. Nihil.
           </span>
+          {carouselData.buttonLink && (
+            <ButtonLink menuCategory={carouselData.buttonLink} />
+          )}
         </div>
       </>
     );
@@ -92,9 +106,7 @@ export const Carousel = () => {
     <>
       <div className="overflow-hidden grid grid-cols-1 md:grid-cols-2">
         <div
-          className={
-            "flex ease-in-out carousel-animate -translate-x-ful min-w-full"
-          }
+          className={"flex ease-in-out carousel-animate min-w-full"}
           style={{ transform: `translateX(-${isShowing * 200}%)` }}
         >
           {images.map(mapCarousel)}
